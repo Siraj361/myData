18ba193843ba888fda34695cf27cd9d4fdd8f785
Siraj Ahmed <165899659+Bijaran@users.noreply.github.com>
2025-06-16 16:54:39 +0500

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,42 @@
+
+// var laptop ;
+
+// laptop = document.getElementById("three").getAttributeNode('style');
+// laptop = document.getElementById("three").getAttributeNode('class');
+// laptop = document.getElementById("three").getAttribute('class');
+// laptop = document.getElementById("three").attributes;
+// laptop = document.getElementById("three").attributes[2];
+// laptop = document.getElementById("three").attributes[2].value;
+// laptop = document.getElementById("three").attributes[2].name;
+
+
+// function AlishaQudisia()
+// {
+// 	var p = document.getElementById('three');
+
+//       p.innerText = "Ghania Hussain";
+//       p.style.color ="red";
+//       p.style.fontSize="40px";
+// 	}
+
+      // var createElement = document.createElement("h1");
+      // createElement.innerText ="My name is Rida Memon";
+      // createElement.style.color="blue";
+      // document.getElementById('siraj').appendChild(createElement)
+
+
+      // console.log(createElement);
+
+
+// var tag = document.createElement("p")
+
+
+// console.log(tag)
+
+// var tag = document.getElementById("three");
+// tag.setAttribute("class", "siraj");
+
+// var tag = document.getElementById("three").getAttribute('class');
+
+
+// console.log(tag);
